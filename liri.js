b55903c2415188df802a3ab3329a7e8c2bbfb1d5
mkkiyoi <mkkiyoi@gmail.com>
2019-10-26 13:25:59 -0700

    Refactored concert-this case to use axiosCall helper function.

--- liri.js
+++ liri.js
@@ -57,7 +57,7 @@ switch (command) {
     case 'concert-this':
         let artist = checkForInput();
         let queryURL = 'https://rest.bandsintown.com/artists/' + artist + '/events?app_id=codingbootcamp';
-        axios.get(queryURL).then((response) => {
+        axiosCall(command, movie).then(response => {
             console.log('Events for ' + artist.replace('+', ' ') + ':');
             console.log('---------------------------------------------------');
             for(let i = 0; i < 10; i++) {
@@ -73,23 +73,7 @@ switch (command) {
                 console.log('---------------------------------------------------');
             }
         }).catch(function(error) {
-            if (error.response) {
-              // The request was made and the server responded with a status code
-              // that falls out of the range of 2xx
-              console.log('---------------Data---------------');
-              console.log(error.response.data);
-              console.log('---------------Status---------------');
-              console.log(error.response.status);
-              console.log('---------------Status---------------');
-              console.log(error.response.headers);
-            } else if (error.request) {
-              // The request was made but no response was received
-              // `error.request` is an object that comes back with details pertaining to the error that occurred.
-              console.log(error.request);
-            } else {
-              // Something happened in setting up the request that triggered an Error
-              console.log('Error', error.message);
-            }
+            console.log(error)
         });
         break;
     case 'spotify-this-song':
